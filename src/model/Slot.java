c4fb858f4f6dec93d2514a94f1c06a78a69b3353
MGMaikeru <13miguegonza2005@gmail.com>
2023-03-08 21:58:23 -0500

    Fix missing slot class

--- src/model/Slot.java
+++ src/model/Slot.java
@@ -2,16 +2,56 @@ package model;
 
 public class Slot {
 
+	private Slot previous, next;
 	private int slotNumber;
 	private String snake;
 	private String ladder;
 
 	/**
-	 * 
+	 *
 	 * @param slotNumber
 	 */
 	public Slot(int slotNumber) {
+		this.slotNumber = slotNumber;
+	}
 
+	/**
+	 * @return Slot
+	 */
+	public Slot getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(Slot previous) {
+		this.previous = previous;
+	}
+
+	public Slot getNext() {
+		return next;
+	}
+
+	public void setNext(Slot next) {
+		this.next = next;
+	}
+
+	public int getSlotNumber() {
+		return slotNumber;
+	}
+
+	public String getSnake() {
+		return snake;
+	}
+
+	public void setSnake(String snake) {
+		this.snake = snake;
+	}
+
+	public String getLadder() {
+		return ladder;
+	}
+
+	public void setLadder(String ladder) {
+		this.ladder = ladder;
 	}
 
 }
